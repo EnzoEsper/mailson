74a39705ed69da2cebea4218b357d59da334ed00
enzoesper <enzoesper@hotmail.com>
2019-08-28 11:32:45 -0300

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+if (procces.env.NODE_ENV === "production") {
+  // we are un production, return the prod set of keys
+  module.exports = require("./prod");
+} else {
+  // we are in dev, return the dev set of keys
+  module.exports = require("./dev");
+}
