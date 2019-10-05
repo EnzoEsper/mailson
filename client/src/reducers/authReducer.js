bbf4dc92ff69b55b59fe0e558c84797acb46b994
enzoesper <enzoesper@hotmail.com>
2019-10-05 20:11:01 -0300

    adding the fetch user action type to the reducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,11 @@
-export default (state = {}, action) => {
-  console.log(action);
+import { FETCH_USER } from "../actions/types";
+
+export default (state = null, action) => {
   switch (action.type) {
+    case FETCH_USER:
+      // si el payload es un string vacio retorna false
+      // si es el objeto con los datos del usuario logeado, retorna el objeto (user model)
+      return action.payload || false;
     default:
       return state;
   }
