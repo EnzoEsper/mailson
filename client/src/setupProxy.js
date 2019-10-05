5a61c9d86a4f906a7672358c9b611eb8fc1d2d97
enzoesper <enzoesper@hotmail.com>
2019-10-05 01:04:58 -0300

    adding another path to the proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -5,9 +5,8 @@
 // }
 // this code replaces the code from above that earlier went in the package.json file
 
-const proxy = require('http-proxy-middleware');
+const proxy = require("http-proxy-middleware");
 module.exports = function(app) {
-    app.use(proxy('/auth/google', 
-        { target: 'http://localhost:5000/' }
-    ));
-}
+  app.use(proxy("/auth/google", { target: "http://localhost:5000/" })),
+    app.use(proxy("/api/*", { target: "http://localhost:5000/" }));
+};
