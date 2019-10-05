3f5124b5241d1cf862a70f45b7853d38e7238603
enzoesper <enzoesper@hotmail.com>
2019-10-05 20:10:18 -0300

    conditionally render the content of the header depending if the user is logged in or logged out

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,42 @@
 import React from "react";
+import { connect } from "react-redux";
 
 class Header extends React.Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      // el usuario no esta logeado
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
+      // el usario esta logeado (entra por el default cuando es el objeto (user model))
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="left brand-logo">Mailson</a>
-          <ul className="right">
-            <li>
-              <a>Login with Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = state => {
+  return { auth: state.auth };
+};
+
+export default connect(mapStateToProps)(Header);
