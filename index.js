1be90e48173cbcc5f9ca5afa8f68e42f2cc209b1
enzoesper <enzoesper@hotmail.com>
2019-10-06 21:49:11 -0300

    adding code to allow express to handle any requests that comes to a routehandler that doesnt know and require some assets related to the client side of the application

--- index.js
+++ index.js
@@ -28,6 +28,22 @@ app.use(passport.session());
 // inmediately calls the functions exported from the authRoutes file with the app ass argument
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+
+// Si se esta en produccion (heroku en este caso)
+//
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets (like our main.js or main.css files)
+  app.use(express.static("client/build")); // if any request comes in for some route/file/anything and we dont uderstand what to
+  //looking for, then look into the client/build directory and try to see if there is some file inside there that matches up with what the request is looking for
+
+  // Express will serve up the index.html file if it doesnt recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    // if someone makes a request for a route that we not understand, just serve the html document
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // heroku inject the env variable PORT
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
